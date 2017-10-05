90faf1ed0900d68c6cd7a2f1e2b59c30bc0fc985
Paras-C <paras_c@hotmail.co.uk>
2017-10-06 00:50:59 +0100

    Meh

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Todo.create!(todo_item: "Test1")
+Todo.create!(todo_item: "Test2")
+Todo.create!(todo_item: "Test3")
+Todo.create!(todo_item: "Test4")
